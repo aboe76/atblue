127d5d39dd1f2f3950b4a861fb5946615c601833
Niels Abspoel <n.abspoel@atcomputing.nl>
2025-11-27 13:49:43 +0100

    test horizon with new crypto library

---
+++ files/scripts/horizon.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+rpm -Uvh --noscripts https://download3.omnissa.com/software/CART26FQ2_LIN64_RPMPKG_2506/Omnissa-Horizon-Client-2506-8.16.0-16536624989.x64.rpm
+
+mv /usr/lib/omnissa/libcrypto.so.3 /usr/lib/omnissa/libcrypto.so.3.old
+
+ln -s /usr/lib64/libcrypto.so.3 /usr/lib/omnissa/libcrypto.so.3
+
+
