5fc06dd229f586f91800709d2a76bc1a7c9ef43c
Niels Abspoel <n.abspoel@atcomputing.nl>
2025-12-02 15:03:37 +0100

    test older client

--- files/scripts/horizon.sh
+++ files/scripts/horizon.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-rpm -Uvh --noscripts https://download3.omnissa.com/software/CART26FQ2_LIN64_RPMPKG_2506/Omnissa-Horizon-Client-2506-8.16.0-16536624989.x64.rpm
+#rpm -Uvh --noscripts https://download3.omnissa.com/software/CART26FQ2_LIN64_RPMPKG_2506/Omnissa-Horizon-Client-2506-8.16.0-16536624989.x64.rpm
+rpm -Uvh --noscripts https://download3.omnissa.com/software/CART25FQ4_LIN64_RPMPkg_2412/Omnissa-Horizon-Client-2412-8.14.0-12437214089.x64.rpm
 
-mv /usr/lib/omnissa/libcrypto.so.3 /usr/lib/omnissa/libcrypto.so.3.old
-mv /usr/lib/omnissa/libssl.so.3 /usr/lib/omnissa/libssl.so.3.old
-mv /usr/lib/omnissa/libcurl.so.4 /usr/lib/omnissa/libcurl.so.4.old
+#mv /usr/lib/omnissa/libcrypto.so.3 /usr/lib/omnissa/libcrypto.so.3.old
+#mv /usr/lib/omnissa/libssl.so.3 /usr/lib/omnissa/libssl.so.3.old
+#mv /usr/lib/omnissa/libcurl.so.4 /usr/lib/omnissa/libcurl.so.4.old
 
-ln -s /usr/lib64/libcrypto.so.3 /usr/lib/omnissa/libcrypto.so.3
-ln -s /usr/lib64/libssl.so.3 /usr/lib/omnissa/libssl.so.3
-ln -s /usr/lib64/libcurl.so.4 /usr/lib/omnissa/libcurl.so.4
+#ln -s /usr/lib64/libcrypto.so.3 /usr/lib/omnissa/libcrypto.so.3
+#ln -s /usr/lib64/libssl.so.3 /usr/lib/omnissa/libssl.so.3
+#ln -s /usr/lib64/libcurl.so.4 /usr/lib/omnissa/libcurl.so.4
 
 
