dc2b06307d8ed9c110ee3f6e16f3ae3ad0e87757
Niels Abspoel <aboe76@gmail.com>
2026-04-28 21:33:56 +0200

    test rpm script

---
+++ files/scripts/displaylinkrpm.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+rpm -Uvh --noscripts https://github.com/displaylink-rpm/displaylink-rpm/releases/download/v6.2.0/fedora-43-displaylink-1.14.15-1.github_evdi.x86_64.rpm
